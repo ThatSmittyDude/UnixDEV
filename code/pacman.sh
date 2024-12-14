6e3b0c67af67a4ef095ae815e526edc61ffef091
that_smitty_dude <ThatSmittyDude@outlook.com>
2024-12-14 01:20:15 -0500

    Delete code directory
    Delete old code for updated version

--- code/pacman.sh
+++
@@ -1,2 +0,0 @@
-sudo pacman -Syu -y
-sudo pacman -S nasm yasm gcc gdb gcc-fortran btop htop tree zip unzip openssh base-devel texinfo nmap net-tools tmux zsh ksh binwalk
--- code/pkg.sh
+++
@@ -1,3 +0,0 @@
-sudo pkg update
-sudo pkg upgrade -y
-sudo pkg install btop htop gcc gdb nasm yasm tree nmap zip unzip z80asm zsh ksh binwalk
