fd60727e3cb1f3dbe6813462d0e7de150c7d0f93
that_smitty_dude <ThatSmittyDude@outlook.com>
2024-12-14 12:51:57 -0500

    Delete cpp directory

--- cpp/unix/unix.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <ctime>
-
-int main(){
-	std::time_t result = std::time(nullptr);
-	std::cout << "\n" << "UNIX Timestamp: " << result << "\n" << std::endl;
-	return 0;
-}
-
